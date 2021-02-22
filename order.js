db3d6ba3db00bab6f0aff10a08195270baed6197
gerberbaby <jungyu2jack@gmail.com>
2021-02-22 15:14:59 -0600

    added messages section to order.js and style def to common.css

--- order.js
+++ order.js
@@ -2,7 +2,7 @@ API.load = (urlParams) => {
   Render.loading('main');
   API.call({
     cacheMS: 60000, // 60s - customers don't have access to 'live' data for load issues.
-    method: 'v2-getCustomerOrder',
+    method: 'v2-getCustomerOrder-dev',
     onSuccess: (data) => {
       $('#main').html(Render.try('main',data));
     },
@@ -19,7 +19,7 @@ API.load = (urlParams) => {
   });
 };
 
-Render.main = (data) => {
+Render.main = (data) => {   //added messages section (jack)
   var result = `
     ${Render.try('header', data)}
     <h3 id="top">Order Details</h3>
@@ -33,9 +33,112 @@ Render.main = (data) => {
 
     <h3 id="proofs">Proofs</h3>
     ${Render.try('proofs', data)}
+
+    <h3 id="messages">Message History</h3>
+    <div class="section">
+      ${Render.try('messages', data)}
+    </div>
   `;
   return result;
 };
+
+Render.messages = (data) => {//<small style="top: 0;" class="datetime">${item.created}</small>
+  var result = '';
+  data.order_messages.forEach((item) => {
+    var msgDate = new Date(item.created);
+    var day = msgDate.getDate();
+    switch(day) {
+      case 0: day = "00"; break; case 1: day = "01"; break; case 2: day = "02"; break; case 3: day = "03"; break; case 4: day = "04"; break; case 5: day = "05"; break; case 6: day = "06"; break; case 7: day = "07"; break; case 8: day = "08"; break; case 9: day = "09"; break;
+    }
+    var month = msgDate.getMonth();
+    switch(month) {
+      case 0: month = "Jan"; break; case 1: month = "Feb"; break; case 2: month = "Mar"; break; case 3: month = "Apr"; break; case 4: month = "May"; break; case 5: month = "Jun"; break; case 6: month = "Jul"; break; case 7: month = "Aug"; break; case 8: month = "Sep"; break; case 9: month = "Oct"; break; case 10: month = "Nov"; break; case 11: month = "Dec"; break;
+    }
+    var year = msgDate.getFullYear();
+    var time = solveTimeString(msgDate);
+
+    if(item.subject == '' || item.subject == null) {//no subject
+      if(item.from == 'You') {
+        result += `
+        <div style="position: absolute; left: 10px; width: 45px;">
+          <h5 style="margin-top: 2px;">${item.from}
+            <small style="opacity: .7; text-align: center; line-height: 12px;"><br>${month} ${day}<br>${year}<br>${time}</small>
+          </h5>
+        </div>
+        <div class="card fluid" style="
+          margin-left: 60px;
+        ">
+          <div class="section">
+            <small style="top: 0;">via ${item.fromSource}</small>
+              <div style="padding: 10px; word-break: break-all;"
+                <p">${item.html}</p>
+              </div>
+            </div>
+          </div>
+        `
+      }
+      if(item.from != 'You') {
+        result += `
+        <div style="position: absolute; right: 10px; width: 45px;">
+          <h5 style="margin-top: 2px;">${item.from}
+            <small style="opacity: .7; text-align: center; line-height: 12px;"><br>${month} ${day}<br>${year}<br>${time}</small>
+          </h5>
+        </div>
+        <div class="card fluid" style="
+          margin-right: 60px;
+        ">
+          <div class="section">
+            <small style="top: 0;">via ${item.toSource}</small>
+              <div style="padding: 10px; word-break: break-all;"
+                <p>${item.html}</p>
+              </div>
+            </div>
+          </div>
+        `
+      }
+    }else {//has subject line
+      if(item.from == 'You') {
+        result += `
+        <div style="position: absolute; left: 10px; width: 45px;">
+          <h5 style="margin-top: 2px;">${item.from}
+            <small style="opacity: .7; text-align: center; line-height: 12px;"><br>${month} ${day}<br>${year}<br>${time}</small>
+          </h5>
+        </div>
+        <div class="card fluid" style="
+          margin-left: 60px;
+        ">
+          <div class="section">
+            <small style="top: 0;">via ${item.fromSource}</small>
+              <div style="padding: 10px; padding-top: 40px; word-break: break-all;"
+                <p>SUBJECT: ${item.subject}<br>${item.html}</p>
+              </div>
+            </div>
+          </div>
+        `
+      }
+      if(item.from != 'You') {
+        result += `
+        <div style="position: absolute; right: 10px; width: 45px;">
+          <h5 style="margin-top: 2px;">${item.from}
+            <small style="opacity: .7; text-align: center; line-height: 12px;"><br>${month} ${day}<br>${year}<br>${time}</small>
+          </h5>
+        </div>
+        <div class="card fluid" style="
+          margin-right: 60px;
+        ">
+          <div class="section">
+            <small style="top: 0;">via ${item.toSource}</small>
+              <div style="padding: 10px; padding-top: 40px; word-break: break-all;"
+                <p>SUBJECT: ${item.subject}<br><br>${item.html}</p>
+              </div>
+            </div>
+          </div>
+        `
+      }
+    }
+  });
+  return result;
+}
 
 Render.header = (data) => {
   var result = `
@@ -43,6 +146,7 @@ Render.header = (data) => {
       <img id="cfgLogo" src="https://cdn.shopify.com/s/files/1/0060/6725/7434/files/heart.png?v=1607199816">
       <a href="#top" class="button" style="margin-left:60px;">#${data.orderId_raw}</a>
       <a href="#proofs" class="button">Proofs</a>
+      <a href="#messages" class="button">Message History</a>
       <!--<a href="#message" class="button">messages</a>-->
     </header>
   `;
@@ -279,4 +383,22 @@ Render.proof = (proof) => {
       </div>
     </div>
   `;
+}
+
+function solveTimeString(date) {
+  var ampm = "am";
+  var digit = date.getHours();
+  if(digit > 12) {
+    ampm = "pm";
+  }
+  switch(digit) {
+    case 0: digit = "12"; break; case 13: digit = "1"; break; case 14: digit = "2"; break; case 15: digit = "3"; break; case 16: digit = "4"; break; case 17: digit = "5"; break; case 18: digit = "6"; break;  case 19: digit = "7"; break;case 20: digit = "8"; break;case 21: digit = "9"; break;case 22: digit = "10"; break;case 23: digit = "11"; break;
+  }
+  var hour = digit;
+  var digit = date.getMinutes();
+  switch(digit) {
+    case 0: digit = "00"; break; case 1: digit = "01"; break; case 2: digit = "02"; break; case 3: digit = "03"; break; case 4: digit = "04"; break; case 5: digit = "05"; break; case 6: digit = "06"; break; case 7: digit = "07"; break; case 8: digit = "08"; break; case 9: digit = "09"; break;
+  }
+  var min = digit;
+  return result = `${hour}:${min}<br>${ampm}`;
 }
